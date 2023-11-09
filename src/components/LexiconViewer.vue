1d4bfbafba383153e5ec43e88af0151fe48bf94f
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2023-11-08 20:00:56 -0500

    storeToRefs FTW !

--- src/components/LexiconViewer.vue
+++ src/components/LexiconViewer.vue
@@ -1,13 +1,23 @@
 <template>
-	<div>
-		<pre>Oh nooooo</pre>
+	<div class="lexicon">
+		<pre>{{ swedish }}</pre>
 	</div>
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
 import { useBaserowStore } from "stores/baserow";
 
-const { out } = useBaserowStore();
+console.log(process.env.BASEROW_TOKEN);
+
+const { out, swedish } = storeToRefs(useBaserowStore());
 
 console.log(out);
 </script>
+
+<style lang="scss">
+.lexicon {
+	height: 400px;
+	overflow-y: scroll;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,17 +5,17 @@
 			src="~assets/quasar-logo-vertical.svg"
 			style="width: 200px; height: 200px"
 		/>
-		<pre>{{ swedish }}</pre>
 	</q-page>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
 import { useBaserowStore } from "stores/baserow";
 
 console.log(process.env.BASEROW_TOKEN);
 
-const { out, swedish } = useBaserowStore();
+const { out, swedish } = storeToRefs(useBaserowStore());
 
 console.log(out);
 </script>
